002086c42a703a8083727e9c7545d44014a0da76
Sorin VIeriu <sorinvieriu@gmail.com>
2024-03-06 07:44:48 +0200

    feat: new items

--- components/Paragraph/index.js
+++ components/Paragraph/index.js
@@ -1,10 +1,11 @@
 import { getTextAlign } from "utils/fonts";
+import { relativeToAbsoluteUrls } from "utils/relativeToAbsoluteUrls";
 export const Paragraph = ({ textColor, content, textAlign = "left" }) => {
 	return (
 		<p
 			className={`amx-w-5xl mx-auto ${getTextAlign(textAlign)}`}
 			style={{ color: textColor }}
-			dangerouslySetInnerHTML={{ __html: content }}
+			dangerouslySetInnerHTML={{ __html: relativeToAbsoluteUrls(content) }}
 		/>
 	);
 };
